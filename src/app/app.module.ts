ace55ded6c4dea09e30b23ae82bfdb0879a1d103
Matthews1337 <Matheusfonseca1997@hotmail.com>
2023-11-22 23:06:51 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { MainPhotoComponent } from './main-photo/main-photo.component';
+import { ApresentationTextComponent } from './apresentation-text/apresentation-text.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    MainPhotoComponent,
+    ApresentationTextComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
